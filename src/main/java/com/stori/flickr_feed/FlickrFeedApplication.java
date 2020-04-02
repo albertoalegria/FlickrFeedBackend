ac9171b662eb6ddd7787fa42c184047c3f11272f
Beto <Beto@Usuarios-MacBook-Pro.local>
2020-04-01 21:34:53 -0600

    Added dependencies and basic service

---
+++ src/main/java/com/stori/flickr_feed/FlickrFeedApplication.java
@@ -0,0 +1,13 @@
+package com.stori.flickr_feed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlickrFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlickrFeedApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/stori/flickr_feed/application/domain/service/FlickrService.java
@@ -0,0 +1,5 @@
+package com.stori.flickr_feed.application.domain.service;
+
+public interface FlickrService {
+    void login();
+}
